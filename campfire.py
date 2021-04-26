a49dd47c2a2660f2ed7c696192a6ca5baaee6125
ahauge20 <64108426+ahauge20@users.noreply.github.com>
2021-04-26 17:53:49 -0400

    Add files via upload

---
+++ campfire.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+if os.path.isfile('accounts.csv'):
+    df = pd.read_csv('accounts.csv')
+else:
+    df = pd.DataFrame(columns = ['Username', 'Password'])
+
+username = input('Enter a usernmame: ')
+password = input('Enter a password: ')
+dict1 = {'Username': username, 'Password': password}
+df1 = pd.DataFrame(data=dict1, index=range(1))
+df = df.append(df1, verify_integrity=True, ignore_index=True)
+df = df.filter(items=['Username','Password'])
+
+print(df)
+
+df.to_csv(r'C:\Users\augus\Documents\Python Scripts\accounts.csv', columns=['Username','Password'])
