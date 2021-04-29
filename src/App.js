6163d6bafc086e7aa915d8b47b7d8b55a047add7
Rachel Johnson <rjohnsn01@gmail.com>
2021-04-29 19:06:46 -0400

    added 2 more food cams and autosizing of images

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from './components/Home';
 import Register from './components/Register';
 import Login from './components/Login'
 import FoodCam from './components/camView';
+import FoodCam2 from './components/camView2';
+import FoodCam3 from './components/camView3';
 
 class App extends Component {
   render() {
@@ -15,7 +17,9 @@ class App extends Component {
           <Route path="/" exact component={ Home } />
           <Route path="/register" exact component={ Register } />
           <Route path="/login" exact component={ Login } />
-          <Route path="/food-cam" exact component={ FoodCam } />
+          <Route path="/food-cam-a" exact component={ FoodCam } />
+          <Route path="/food-cam-b" exact component={ FoodCam2 } />
+          <Route path="/food-cam-c" exact component={ FoodCam3 } />
         </div>
       </BrowserRouter>  
     );
